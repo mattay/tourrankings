06b02af631ee0a1026f71b14bf9e3a16022a0fc3
Matthew Anderson <mattay@users.noreply.github.com>
2024-07-08 11:55:26 +1000

    Remove main(), cleanRecord outside of browser

--- scrape/races.js
+++ scrape/races.js
@@ -1,60 +1,51 @@
-import puppeteer from "puppeteer";
-import { buildUrl } from "../utils/url.js";
-import Race from "../races/races.js";
+import { urlSections } from "../utils/url.js";
+import { formatDate } from "../utils/string.js";
 
-async function scrapePage(url, year) {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle2" });
+function cleanRecord(year, record) {
+  const raceUrl = record.raceUrl.replace(/\/gc$/, "")
+  const linkSections = urlSections(raceUrl, ["_race", "raceId", "year"]);
+  const raceId = linkSections.raceId + "-" + linkSections.year;
 
-  // Extract data from the specified selector
-  const data = await page.$$eval(
-    ".content table tbody tr",
-    (tableRows, year) => {
-      formatDate = (textDate) => {
-        const [month, day] = textDate.split("."); // Split start date into month and day
-        return `${year}/${month.padStart(2, "0")}/${day.padStart(2, "0")}`; // Format startDate as yyyy/mm/dd
-      };
+  return {
+    ...record,
+    raceId,
+    year,
+    startDate: formatDate(year, record.startDate, "."),
+    endDate: formatDate(year, record.endDate, "."),
+    raceUrl
+  };
+}
 
+export async function scrapeRaces(page, url, year) {
+  try {
+    await page.goto(url, { waitUntil: "networkidle2" });
+
+    // Extract data from the specified selector
+    const data = await page.$$eval(".content table tbody tr", (tableRows) => {
       return tableRows.map((tr) => {
         const tds = tr.querySelectorAll("td");
 
         const dateText = tds[0].textContent.trim();
-        const [startDateText, endDateText] = dateText.split(" - ");
         const raceLink = tds[2].querySelector("a");
+        const raceClass = tds[4].textContent.trim();
+        const [startDateText, endDateText] = dateText.split(" - ");
+        const raceName = raceLink.textContent.trim();
+        const raceUrl = raceLink.href;
 
         return {
-          year,
-          startDate: formatDate(startDateText),
-          endDate: formatDate(endDateText),
-          raceName: raceLink.textContent.trim(), // Get the race name
-          raceUrl: raceLink.href.replace(/\/gc$/, ""), // Get the href attribute of the race link
-          class: tds[4].textContent.trim(),
+          raceName,
+          class: raceClass,
+          startDate: startDateText,
+          endDate: endDateText,
+          raceUrl,
         };
       });
-    },
-    year,
-  );
+    });
 
-  // Close the browser
-  await browser.close();
-  return data;
+    // Cleanup records - eval happens within browser.
+    return data.map((record) => cleanRecord(year, record));
+  } catch (exception) {
+    console.error(exception.error, exception.message);
+    return null;
+  }
 }
-
-async function main() {
-  const filter = {
-    year: 2024,
-    circuit: 1,
-    class: "2.UWT",
-    filter: "Filter",
-    p: "uci",
-    s: "year-calendar",
-  };
-  const url = buildUrl("https://www.procyclingstats.com/races.php", filter);
-  const tableRows = await scrapePage(url, filter.year);
-  console.log(tableRows[0]);
-  const races = new Race();
-  races.update(tableRows);
-}
-
-main().catch(console.error);

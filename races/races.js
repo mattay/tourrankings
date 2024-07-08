f56afb5a28558cc0a910d0812310abe5f4c29a92
Matthew Anderson <mattay@users.noreply.github.com>
2024-07-08 12:13:50 +1000

    Data Models - Race records

--- races/races.js
+++ races/races.js
@@ -2,20 +2,60 @@ import CSVdataModel from "../utils/dataModel_csv.js";
 
 class Race extends CSVdataModel {
   constructor() {
-    super("races/data_races.csv", "raceUrl");
+    super("races/races_data.csv", ["raceId"]);
     this.csvHeaders = [
       "Year",
-      "StartDate",
-      "EndDate",
-      "RaceName",
-      "RaceUrl",
+      "Start Date",
+      "End Date",
       "Class",
+      "Race Id",
+      "Race Name",
+      "Race Url",
     ];
   }
 
   sortRows() {
     this.rows.sort((a, b) => {
       return new Date(a.startDate) - new Date(b.startDate);
+    });
+  }
+
+  async past() {
+    await this.read(); // Refresh
+    const today = new Date();
+
+    return this.rows.filter((record) => {
+      const raceEndDate = new Date(record.endDate);
+      return today <= raceEndDate;
+    });
+  }
+
+  async inProgress() {
+    await this.read(); // Refresh
+    const today = new Date();
+
+    return this.rows.filter((record) => {
+      const raceStartDate = new Date(record.startDate);
+      const raceEndDate = new Date(record.endDate);
+      return today >= raceStartDate && today <= raceEndDate;
+    });
+  }
+
+  async upcoming() {
+    await this.read(); // Refresh
+    const today = new Date();
+
+    return this.rows.filter((record) => {
+      const raceStartDate = new Date(record.startDate);
+      return today <= raceStartDate;
+    });
+  }
+
+  async racesInYear(year) {
+    await this.read(); // Refresh
+
+    return this.rows.filter((record) => {
+      return record.year == year;
     });
   }
 }

4c38d600e42552e52b73b979c426f2e4b1f4d414
Matthew Anderson <mattay@users.noreply.github.com>
2024-07-12 11:59:42 +1000

    jsDoc added to functions

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,21 @@
+/**
+ * Generates a random integer within a specified range.
+ *
+ * @param {number} min - The minimum value of the range (inclusive).
+ * @param {number} max - The maximum value of the range (inclusive).
+ * @returns {number} - A random integer between min and max (inclusive).
+ */
 export function randomFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // https://stackoverflow.com/a/51200664/5250085
+/**
+ * Pauses execution for a specified number of milliseconds.
+ *
+ * @param {number} [milliseconds=500] - The number of milliseconds to sleep. Defaults to 500 milliseconds.
+ * @returns {Promise<void>} - A promise that resolves after the specified delay.
+ */
 export function sleep(milliseconds = 500) {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }

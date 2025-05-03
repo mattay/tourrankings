0b276e453371e3f64dc50b5e3c0e0d037dadd309
Matthew Anderson <mattay@users.noreply.github.com>
2025-05-03 10:07:42 +1000

    Exposing typedefs from dataService

--- src/services/dataServiceInstance.js
+++ src/services/dataServiceInstance.js
@@ -1,5 +1,15 @@
 import DataService from "./data/dataService.js";
 
+/**
+ * Classes
+ * @typedef {import('./data/dataService').RaceData} RaceData
+ * @typedef {import('./data/dataService').RaceStageData} RaceStageData
+ * @typedef {import('./data/dataService').RaceStageResultData} RaceStageResultData
+ * @typedef {import('./data/dataService').RaceRiderData} RaceRiderData
+ * @typedef {import('./data/dataService').TeamData} TeamData
+ * @typedef {import('./data/dataService').RiderData} RiderData
+ */
+
 const dataService = new DataService();
 
 export default dataService;

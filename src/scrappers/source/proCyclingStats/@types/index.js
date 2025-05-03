86677f95ca77d7baba12b7c78e1d82ae3510a957
Matthew Anderson <mattay@users.noreply.github.com>
2025-05-03 14:49:18 +1000

    Titled case types. Added missing properites

--- src/scrappers/source/proCyclingStats/@types/index.js
+++ src/scrappers/source/proCyclingStats/@types/index.js
@@ -1,54 +1,56 @@
 /**
  * @typedef {Object} ScrapedRace
- * @property {string} raceUID - The unique identifier for the race (raceId:year)
- * @property {number} year - The year of the race
- * @property {string} startDate - The start date of the race in YYYY-MM-DD format
- * @property {string} endDate - The end date of the race in YYYY-MM-DD format
- * @property {string} raceName - The name of the race
- * @property {string} raceClass - The classification of the race
- * @property {string} racePcsID - The cleaned URL of the race page
- * @property {string} racePcsUrl - The cleaned URL of the race page
+ * @property {String} raceUID - The unique identifier for the race (raceId:year)
+ * @property {Number} year - The year of the race
+ * @property {String} startDate - The start date of the race in YYYY-MM-DD format
+ * @property {String} endDate - The end date of the race in YYYY-MM-DD format
+ * @property {String} raceName - The name of the race
+ * @property {String} raceClass - The classification of the race
+ * @property {String} racePcsID - The cleaned URL of the race page
+ * @property {String} racePcsUrl - The cleaned URL of the race page
  */
 
 /**
  * @typedef {Object} ScrapedRaceStage
- * @property {string} raceUID - The unique identifier for the race (raceId:year)
- * @property {string} stageUID - The unique identifier for the stage (raceId:year:stageId)
- * @property {number} year - The year of the race
- * @property {string} date - The date of the stage in YYYY-MM-DD format
- * @property {number} stage - The property of the stage
- * @property {string} stageType - The type of the stage
- * @property {string} parcoursType - The type of the stage
- * @property {string} departure - The departure location of the stage
- * @property {string} arrival - The arrival location of the stage
- * @property {number} distance - The distance of the stage in kilometers
- * @property {number} verticalMeters - The elevation of the stage in meters
- * @property {string} stagePcsUrl - The cleaned URL of the stage page
+ * @property {String} raceUID - The unique identifier for the race (raceId:year)
+ * @property {String} stageUID - The unique identifier for the stage (raceId:year:stageId)
+ * @property {Number} year - The year of the race
+ * @property {String} date - The date of the stage in YYYY-MM-DD format
+ * @property {Number} stage - The property of the stage
+ * @property {String} stageType - The type of the stage
+ * @property {String} parcoursType - The type of the stage
+ * @property {String} departure - The departure location of the stage
+ * @property {String} arrival - The arrival location of the stage
+ * @property {Number} distance - The distance of the stage in kilometers
+ * @property {Number} verticalMeters - The elevation of the stage in meters
+ * @property {String} stagePcsUrl - The cleaned URL of the stage page
  */
 
 /**
  * Represents a rider in the race startlist.
  *
  * @typedef {Object} ScrapedRaceStartListRider - Represents a rider in the startlist.
- * @property {number} year - The year of the race.
- * @property {number} bib - The bib number of the rider.
- * @property {string} rider - The name of the rider.
- * @property {string} flag - The flag of the rider's country.
- * @property {string} riderPcsId - The ID of the rider on ProcyclingStats.
- * @property {string} riderPcsUrl - The URL of the rider on ProcyclingStats.
+ * @property {Number} year - The year of the race.
+ * @property {Number} bib - The bib Number of the rider.
+ * @property {String} rider - The name of the rider.
+ * @property {String} flag - The flag of the rider's country.
+ * @property {String} riderPcsId - The ID of the rider on ProcyclingStats.
+ * @property {String} riderPcsUrl - The URL of the rider on ProcyclingStats.
  */
 
 /**
  * Represents a team in the race startlist, including team details and its riders.
  *
  * @typedef {Object} ScrapedRaceStartListTeam - Represents a team in the startlist.
- * @property {number} year - The year of the race.
- * @property {string} teamName - The name of the team.
- * @property {string} teamPcsUrl - The URL of the team on ProcyclingStats.
- * @property {string} jerseyImageUrl - The URL of the team's jersey image.
- * @property {string} teamPcsId - The ID of the team on ProcyclingStats.
- * @property {string} teamClassification - The classification of the team.
- * @property {Array<ScrapedRaceStartListRider>} riders - An array of riders in the team.
+ * @property {Number} year - The year of the race.
+ * @property {String} teamName - The name of the team.
+ * @property {String} teamPcsUrl - The URL of the team on ProcyclingStats.
+ * @property {String} jerseyImageUrl - The URL of the team's jersey image.
+ * @property {String} teamPcsId - The ID of the team on ProcyclingStats.
+ * @property {String} teamClassification - The classification of the team.
+ * @property {ScrapedRaceStartListRider[]} riders - An array of riders in the team.
  *
  * @see ScrapedRaceStartListRider
  */
+
+export {};

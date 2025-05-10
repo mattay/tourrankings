371d4ed4ece8f3a08b6499c779b89e0147e7d756
Matthew Anderson <mattay@users.noreply.github.com>
2025-05-10 18:27:32 +1000

    Removed unused functions

--- src/models/dataModel_csv.js
+++ src/models/dataModel_csv.js
@@ -1,7 +1,7 @@
 import path from "path";
 import fs from "fs";
 import csv from "csv-parser";
-import { toCamelCase, toTitleCase } from "../utils/string.js";
+import { toCamelCase } from "../utils/string.js";
 import { logError, logOut } from "../utils/logging.js";
 import { dirname } from "path";
 
--- src/scrappers/scrape_proCyclingStats.js
+++ src/scrappers/scrape_proCyclingStats.js
@@ -1,4 +1,4 @@
-import puppeteer, { Page } from "puppeteer-core";
+import puppeteer from "puppeteer-core";
 
 // Data Models
 import {

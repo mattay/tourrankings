6a40a47863098ae41a4965df6c169ee0edd87e06
Matthew Anderson <mattay@users.noreply.github.com>
2025-05-24 00:06:56 +1000

    Rider positioing and transitions

--- src/client/components/rider/rider.js
+++ src/client/components/rider/rider.js
@@ -18,6 +18,7 @@
  * @property {d3.ScaleLinear<number, number>} xScale - D3 linear scale for horizontal positioning
  * @property {d3.ScaleLinear<number, number>} yScale - D3 linear scale for vertical positioning
  * @property {Object} offsets - Offsets for layout adjustments (e.g., text offset)
+ * @property {number} stage - Font size for rider labels
  * @property {function(RiderDatum): void} [onRiderClick] - Callback invoked on rider click
  * @property {number} [transitionDuration=750] - Duration for transitions in milliseconds
  */
@@ -32,14 +33,15 @@
  */
 export function createRiderComponent({
   fontHeight = 16,
+  xScale,
   yScale,
   offsets,
+  stage,
   onRiderClick = () => {},
-  transitionDuration = 750,
-} = {}) {
-  if (!yScale) {
-    throw new Error("xScale is required");
-  }
+  transitionDuration = 840,
+}) {
+  if (!xScale) throw new Error("xScale is required");
+  if (!yScale) throw new Error("yScale is required");
 
   /**
    * Initialize entering rider groups by setting attributes, styles, and appending child elements.
@@ -50,14 +52,16 @@ export function createRiderComponent({
     riderEnter
       .attr("class", "rider")
       .style("opacity", 0)
+      .attr("transform", (d, i) => `translate(${xScale(stage)}, ${yScale(i)})`)
       .on("click", (event, d) => onRiderClick(d));
 
     riderEnter
       .append("text")
-      .attr("x", 0) // TODO: Add x position calculation based on stage
-      .attr("y", (y, i) => yScale(i))
+      .attr("x", 0)
+      .attr("y", 0)
       .attr("dy", offsets.text)
-      .attr("font-size", fontHeight)
+      .attr("font-size", 0)
+      .attr("fill", "none")
       .text((d) => d.rider);
   };
 
@@ -69,24 +73,29 @@ export function createRiderComponent({
   const updateRidersGroup = (riderSelection) => {
     riderSelection
       .transition()
+      .delay((d, i) => 420 + i * 5)
       .duration(transitionDuration)
-      // .attr(
-      //   "transform",
-      //   (d) => `translate(${xScale(d.positionX)},${yScale(d.positionY)})`,
-      // )
+      .ease(d3.easeQuadInOut)
+      // TODO: have data ordered by ranking for given stage
+      .attr("transform", (d, i) => `translate(${xScale(stage)}, ${yScale(i)})`)
       .style("opacity", 1)
       .attr("class", (d) => {
         let classes = "rider";
-        // if (d.raced) classes += " raced";
-        // if (d.viewed) classes += " viewed";
         return classes;
       });
+
+    riderSelection
+      .select("text")
+      .transition()
+      .delay((d, i) => 420 + i * 10)
+      .duration(transitionDuration)
+      .ease(d3.easeQuadInOut);
   };
 
   /**
    * Handle exit selection by fading out and removing rider groups.
    *
-   * @param {d3.Selection<SVGGElement, RiderDatum, any, any>} riderExit - D3 selection of rider groups to remove.
+   * @param {d3.Selection<SVGElement, RiderDatum, any, any>} riderExit - D3 selection of rider groups to remove.
    */
   const exitRidersGroup = (riderExit) => {
     riderExit
@@ -99,11 +108,10 @@ export function createRiderComponent({
   /**
    * Main rider component function: binds rider data to selection and manages enter/update/exit lifecycle.
    *
-   * @param {d3.Selection<SVGGElement, unknown, null, undefined>} selection - D3 selection to bind data to.
+   * @param {d3.Selection<SVGElement, unknown, null, undefined>} selection - D3 selection to bind data to.
    * @param {RiderDatum[]} data - Array of rider data objects.
    */
   return function riderComponent(selection, data) {
-    console.log("Rider Component", data);
     // Bind data with key function
     const riders = selection.selectAll("g.rider").data(data, (d) => d.bib);
 

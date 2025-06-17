170ae8c551b9458f2025f04dba6aa364cb184a5f
Matthew Anderson <mattay@users.noreply.github.com>
2025-06-17 21:23:49 +1000

    Remove url update + event dispatch

--- src/client/state/actions/stage.js
+++ src/client/state/actions/stage.js
@@ -1,32 +1,21 @@
-// State Management
 import store from "../storeInstance";
-import { EVENT_TYPES, dispatch } from "../events";
-// Utils
-import { getRaceInfoFromUrlPath } from "../../../../src/client/utils";
 
 /**
- * @typedef {import('../../models/@types/stage.d').RaceStageData} RaceStageData
+ * @typedef {import('../../models/@types/stage').RaceStageData} RaceStageData
  */
 
 /**
  *
  * @param {RaceStageData} stage
+ * @returns {void}
  */
 export function actionSelectStage(stage) {
-  const stageNumber = stage.stage;
-  // Update URL
-  const pathParts = getRaceInfoFromUrlPath();
-  pathParts.stage = stageNumber;
-  const url = "/" + Object.values(pathParts).join("/");
-  window.history.pushState(
-    { stageUID: stage.stageUID },
-    `Stage ${stageNumber}`,
-    url,
-  );
+  if (!stage && stage.stage) {
+    console.warn("Stage is required");
+    return;
+  }
 
   // Update store
+  const stageNumber = stage.stage;
   store.setState({ currentStage: stageNumber });
-
-  // Dispatch event
-  dispatch(EVENT_TYPES.STAGE_CHANGED, stageNumber);
 }

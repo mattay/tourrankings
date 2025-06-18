9bbee43e82f3cb4db75281238b174a057f4ed9c0
Matthew Anderson <mattay@users.noreply.github.com>
2025-06-18 16:11:01 +1000

    fix: Check currentClassification is valid

--- src/client/components/page/classification-tabs.js
+++ src/client/components/page/classification-tabs.js
@@ -1,5 +1,8 @@
 import { actionSelectClassification } from "src/client/state/actions";
-import { CLASSIFICATION_TYPES } from "src/core/cycling/classification/classification";
+import {
+  CLASSIFICATION_TYPES,
+  isValidClassificationType,
+} from "src/core/cycling/classification/classification";
 
 const CLASSIFICATION_ELEMENT_CLASS = {
   CONTAINER: ".tabs-container",
@@ -34,8 +37,11 @@ export function setupClassificationTabs() {
  * @returns {void}
  */
 export function updateClassificationTabs(state) {
-  const currentClassification =
-    state.currentClassification || CLASSIFICATION_TYPES.STAGE;
+  const currentClassification = isValidClassificationType(
+    state.currentClassification,
+  )
+    ? state.currentClassification
+    : CLASSIFICATION_TYPES.STAGE;
 
   const activeTab = document.querySelector(
     `[data-classification="${currentClassification}"]`,

6de8f34cd406c0fe5bf9c405136f84490c1f52cc
Matthew Anderson <mattay@users.noreply.github.com>
2025-06-24 13:12:13 +1000

    refactor(custom state errors): Improve stateCheckSelected argument interface

--- src/client/state/store/stateCheck.js
+++ src/client/state/store/stateCheck.js
@@ -18,30 +18,30 @@ function validateStateInitialized(state) {
 }
 
 /**
- * @param {State} state - The application state
+ * @typedef {Object} Selected
+ * @param {boolean} race - Check for current race
+ * @param {boolean} year - Check for current stage
  * @param {boolean} stage - Check for current stage
  * @param {boolean} classification - Check for current classification
+ */
+
+/**
+ * @param {State} state - The application state
+ * @param {Selected} selected - Check for current selected
  * @throws {StateNotInitializedError|StatePropertyNotDefinedError|StatePropertyNotSetError} Throws an error if the state is undefined, currentStage is not set, or currentClassification is not set.
  */
-export const stateCheckSelected = (
-  state,
-  stage = false,
-  classification = false,
-) => {
+export const stateCheckSelected = (state, selected) => {
   validateStateInitialized(state);
 
-  // Check state properties exist
-  if (!Object.hasOwn(state, "currentStage"))
-    throw new StatePropertyNotDefinedError(state, "currentStage");
-  if (!Object.hasOwn(state, "currentClassification"))
-    throw new StatePropertyNotDefinedError(state, "currentClassification");
-
-  // Check values we need are set
-  if (stage && state.currentStage == null) {
-    throw new StatePropertyNotSetError(state, "currentStage");
-  }
-  if (classification && state.currentClassification == null) {
-    throw new StatePropertyNotSetError(state, "currentClassification");
+  for (const key in selected) {
+    // Check state properties exist
+    if (!Object.hasOwn(state, key)) {
+      throw new StatePropertyNotDefinedError(state, key);
+    }
+    // Check values we need are set
+    if (selected[key] && state[key] == null) {
+      throw new StatePropertyNotSetError(state, key);
+    }
   }
 
   return true;

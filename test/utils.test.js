a2ff8c94e9f788249be4699fdf8c91a11061ad37
Matthew Anderson <mattay@users.noreply.github.com>
2025-07-04 13:15:26 +1000

    Add test cases for edge cases and more input formats

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,11 +6,16 @@ describe("String utilities", () => {
   it("should convert string to camelCase", () => {
     expect(toCamelCase("hello world")).toBe("helloWorld");
     expect(toCamelCase("test_string")).toBe("testString");
+    expect(toCamelCase("HELLO WORLD")).toBe("helloWorld");
+    expect(toCamelCase("test-string")).toBe("testString");
+    expect(toCamelCase("")).toBe("");
   });
 
   it("should convert first character to uppercase", () => {
     expect(toTitleCase("hello")).toBe("Hello");
     expect(toTitleCase("test")).toBe("Test");
+    expect(toTitleCase("")).toBe("");
+    expect(toTitleCase("A")).toBe("A");
   });
 });
 
@@ -18,5 +23,7 @@ describe("Time utilities", () => {
   it("should convert time string to seconds", () => {
     expect(stringToSeconds("01:30:45")).toBe(5445);
     expect(stringToSeconds("00:05:30")).toBe(330);
+    expect(stringToSeconds("00:00:01")).toBe(1);
+    expect(stringToSeconds("10:00:00")).toBe(36000);
   });
 });

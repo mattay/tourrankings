bcb36aebbb404182ba2c4a89803e6df7524b1a01
Matthew Anderson <mattay@users.noreply.github.com>
2025-09-02 15:28:42 +1000

    Add data param to logger functions for debug values

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -8,10 +8,18 @@ const PADDINGDOMAIN = 32;
  * @param {string} domain - The category or source of the log message.
  * @param {string} message - The message to log.
  * @param {"log" | "debug" | "info" | "warn" | "error"} [logLevel="log"] - The log level (defaults to "log").
+ * @param {any} [data] - Additional data to log.
  */
-export function logOut(domain, message, logLevel = "log") {
+export function logOut(domain, message, logLevel = "log", data) {
   const timestamp = isoDateTime(new Date());
   console[logLevel](`${timestamp} ${domain.padEnd(PADDINGDOMAIN)} ${message}`);
+
+  if (typeof data !== "undefined") {
+    console[logLevel](
+      `${timestamp} ${domain.padEnd(PADDINGDOMAIN)} [DEBUG VALUE]`,
+      data,
+    );
+  }
 }
 
 /**
@@ -19,9 +27,10 @@ export function logOut(domain, message, logLevel = "log") {
  *
  * @param {string} domain - The category or source of the error message.
  * @param {string} message - The error message to log.
- * @param {Error|null} error - The error object to log.
+ * @param {Error|null} [error] - The error object to log.
+ * @param {any} [data] - Additional data to log.
  */
-export function logError(domain, message, error = null) {
+export function logError(domain, message, error = null, data) {
   const timestamp = isoDateTime(new Date());
   console.error(
     `${timestamp} ${domain.padEnd(PADDINGDOMAIN)} [ERROR] ${message}`,
@@ -32,6 +41,12 @@ export function logError(domain, message, error = null) {
     );
     console.error(
       `${timestamp} ${domain.padEnd(PADDINGDOMAIN)} [ERROR MESSAGE] ${error.message}`,
+    );
+  }
+  if (typeof data !== "undefined") {
+    console.error(
+      `${timestamp} ${domain.padEnd(PADDINGDOMAIN)} [DEBUG VALUE]`,
+      data,
     );
   }
 }
